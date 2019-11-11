76e58200ab83d31565d6853944d0b9254bfd53aa
Devin Jacobs <dbuttonz@gmail.com>
2019-11-10 22:48:46 -0700

    Gruvbox theme and undo-tree

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+(package-initialize)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+
+(add-to-list 'load-path "~/.emacs.d/undo-tree")
+(require 'undo-tree)
+
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+(setq package-selected-packages '(gruvbox-theme))
+
+(load-theme 'gruvbox-dark-hard 1)
