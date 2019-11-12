97fa84ae804f59a6dfe6eecf536db49e6eee265c
Devin Jacobs <dbuttonz@gmail.com>
2019-11-12 00:05:12 -0700

    Ivy keybindings

--- init.el
+++ init.el
@@ -22,8 +22,18 @@
 	yasnippet))
 
 (evil-mode 1)
-(ivy-mode 1)
 
+; ivy config
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers 1)
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+; keyfreq config
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
 (setq keyfreq-file "~/.emacs.d/keyfreq")
