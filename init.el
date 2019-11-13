e820c2aa18cadea937552c9def5ed57cd283f848
Devin Jacobs <dbuttonz@gmail.com>
2019-11-13 11:08:51 -0700

    scroll-conservatively 100

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
 (setq keyfreq-file-lock "~/.emacs.d/keyfreq-lock")
 
 (setq scroll-margin 2)
-(setq scroll-conservatively 10)
+(setq scroll-conservatively 100)
 
 (load-theme 'gruvbox-dark-hard 1)
 (set-face-attribute 'default nil :height 140)
