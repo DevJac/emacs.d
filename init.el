5dea8e5b105a60b60a47c7508de300e8cab100ac
Devin Jacobs <dbuttonz@gmail.com>
2019-12-21 14:05:06 -0700

    Remove workarounds for Emacs 26.2

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-;; The following line is a workaround for Emacs 26.2.
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
 (package-initialize)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
