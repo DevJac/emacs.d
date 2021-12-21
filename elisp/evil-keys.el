45ffa17329f0fe391e10aacf6c4211942e5cc79d
DevJac <dbuttonz@gmail.com>
2021-12-21 11:55:57 -0700

    Cleanup evil-keys

--- elisp/evil-keys.el
+++ elisp/evil-keys.el
@@ -1,6 +1,13 @@
 (evil-ex-define-cmd "q[uit]" #'kill-buffer-and-window)
+
+;; normal state has precedence over motion state,
+;; so we have to clear the normal state key
+;; for our motion state key to be seen.
 (evil-define-key 'normal 'global
+  (kbd "Z Z") nil)
+(evil-define-key 'motion 'global
   (kbd "Z Z") #'save-then-kill-buffer-and-window)
+
 (evil-define-key 'motion 'global
   ;; (C-x C-f), (C-x C-b), and (C-x k), are basic Emacs keys and should be used.
   ;; This will ensure I remain somewhat comfortable in vanilla Emacs.
@@ -43,19 +50,14 @@
   (kbd "SPC o c g") #'org-clock-goto
   (kbd "SPC o c l") #'org-clock-last
   (kbd "SPC o c d") #'org-clock-display
-  (kbd "SPC o c r") #'org-clock-report
-  ;; neotree
-  (kbd "SPC q q") #'neotree-toggle
-  (kbd "SPC q d") #'neotree-dir)
-(evil-define-key 'normal neotree-mode-map  ; We have to use 'normal map here?
-  (kbd "a") #'neotree-hidden-file-toggle
-  (kbd "d") #'neotree-change-root
-  (kbd "TAB") #'neotree-quick-look
-  (kbd "RET") #'neotree-enter)
-(evil-define-key 'motion Info-mode-map
-  (kbd "SPC") nil   ; Why is this needed here, but not in other modes?
-  (kbd "SPC SPC") (lookup-key Info-mode-map (kbd "SPC")))
+  (kbd "SPC o c r") #'org-clock-report)
+
 (evil-define-key 'motion markdown-mode-map
   (kbd "TAB") #'markdown-cycle)
+
+(evil-define-key 'motion Info-mode-map
+  (kbd "SPC") nil
+  (kbd "SPC SPC") (lookup-key Info-mode-map (kbd "SPC")))
 (evil-define-key 'motion messages-buffer-mode-map
+  (kbd "SPC") nil
   (kbd "SPC SPC") (lookup-key messages-buffer-mode-map (kbd "SPC")))
