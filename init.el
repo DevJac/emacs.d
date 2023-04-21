9315bb14df9a073d9b49e95c7456de151768cefa
DevJac <dbuttonz@gmail.com>
2023-04-21 00:08:37 -0600

    Fix colors

--- init.el
+++ init.el
@@ -38,3 +38,22 @@
 (use-package evil
   :config
   (evil-mode 1))
+
+;;; Customs
+;; Darkened highlight and region backgrounds by #101010
+;; Removed region distant-foreground; it was an ugly bright color
+;; Lightened comment face by #101010
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "#8c7f74"))))
+ '(highlight ((t (:background "#403935"))))
+ '(region ((t (:extend t :background "#403935" :distant-foreground nil)))))
