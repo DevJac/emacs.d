fca4a72b48d311c549e311adcd8b21cb9c203ce9
DevJac <dbuttonz@gmail.com>
2023-04-20 21:14:52 -0600

    use-package, evil

--- init.el
+++ init.el
@@ -12,3 +12,17 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+;;; straight integration with use-package
+;; See: https://github.com/radian-software/straight.el#integration-with-use-package
+;; We want all normal (use-package ...) expressions to use straight by default
+(setq straight-use-package-by-default t)
+;; Before we can use normal (use-package ...) expressions, we need to get use-package
+(straight-use-package 'use-package)
+
+;;; use-package's
+;; :init runs before the package is loaded
+;; :config runs after the package is loaded
+(use-package evil
+  :config
+  (evil-mode 1))
