38af190f73b1fe4998719aa87fa2c1a90795b646
DevJac <dbuttonz@gmail.com>
2025-04-24 22:05:30 -0600

    Packages: consult, vertico, orderless, marginalia, magit, expreg

--- init.el
+++ init.el
@@ -2,45 +2,56 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-;;; Built-in Emacs customizations
-
 ;; Store customizations in another file to keep init.el clean
 (setq custom-file (locate-user-emacs-file "custom-sets.el"))
 (load custom-file 'noerror 'nomessage)
-
 ;; GUI cleanup
 (tool-bar-mode -1)
-;; (menu-bar-mode -1)
-;; (scroll-bar-mode -1)
-
-;; Better completions
-;; See: https://robbmann.io/posts/emacs-29-completions/
+;; Better built-in completions; see: https://robbmann.io/posts/emacs-29-completions/
 (setq completion-auto-help 'visible)
 (setq completion-auto-select 'second-tab)
+;; TAB first tries to indent, then tries to complete
 (setq tab-always-indent 'complete)
-
 ;; No weird files
 (auto-save-visited-mode 1)
 (setq make-backup-files nil)
-
-;; Remember...
 ;; Remember recently opened files
 (recentf-mode 1)
 ;; Remember recent minibuffer entries, such as M-x commands
 (savehist-mode 1)
 ;; Remember cursor location in previously opened files
 (save-place-mode 1)
-
 ;; Automatically update buffers if the underlying file changes
 (global-auto-revert-mode 1)
-
 ;; Highlight trailing whitespace; it's buffer local so we change the default
 (setq-default show-trailing-whitespace t)
-
 ;; Show match count in isearch
 (setq isearch-lazy-count t)
-
 ;; Profile startup with use-package; run use-package-report to see results
 ;; (setq use-package-compute-statistics t)
 
-;;; Third-party packages
+(use-package consult
+  :ensure t
+  :config
+  (consult-customize
+   consult-buffer
+   :preview-key '(:debounce 0.5 any)))
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
+(use-package orderless
+  :ensure t
+  :custom
+  ;; These customizations are suggested by the orderless docs.
+  ;; Apparently file completion in TRAMP must use basic; see docs for more info.
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode 1))
+(use-package magit
+  :ensure t)
+(use-package expreg
+  :ensure t)
