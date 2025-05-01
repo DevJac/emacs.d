8854799c7117a3177971778d911ce07ce04dfb08
DevJac <dbuttonz@gmail.com>
2025-05-01 12:28:49 -0600

    my/enable-babel-evaluate

--- lisp/my.el
+++ lisp/my.el
@@ -2,3 +2,7 @@
   (interactive)
   (let ((completion-in-region-function #'consult-completion-in-region))
     (completion-at-point)))
+
+(defun my/enable-babel-evaluate ()
+  (interactive)
+  (setq org-confirm-babel-evaluate nil))
