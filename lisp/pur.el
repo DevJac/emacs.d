715220b4c5362be2c6d7e9c33dc42c35f6f27db7
DevJac <dbuttonz@gmail.com>
2025-05-04 11:45:48 -0600

    pur keys and cursor customization

--- lisp/pur.el
+++ lisp/pur.el
@@ -4,7 +4,10 @@
 (define-minor-mode pur-mode
   "A lightweight modal editing mode."
   :lighter " PUR"
-  :keymap pur-mode-map)
+  :keymap pur-mode-map
+  (if pur-mode
+      (setq cursor-type 'box)
+    (setq cursor-type 'bar)))
 
 (defun pur-enter ()
   "Enter `pur-mode'."
@@ -28,6 +31,23 @@
     (call-interactively #'kill-region))
   (pur-exit))
 
+(defun pur-insert-above ()
+  (interactive)
+  (call-interactively #'move-beginning-of-line)
+  (call-interactively #'newline)
+  (call-interactively #'previous-line)
+  (pur-exit))
+
+(defun pur-insert-below ()
+  (interactive)
+  (call-interactively #'move-end-of-line)
+  (call-interactively #'newline)
+  (pur-exit))
+
+(defun pur-g ()
+  (interactive)
+  (deactivate-mark))
+
 (defun pur--fill-typeable-keys (map)
   "Bind typeable characters in MAP to `ignore'."
   (dotimes (i (- 127 32))
@@ -40,15 +60,19 @@
 (define-key global-map (kbd "<escape>") #'pur-escape)
 (define-key pur-mode-map (kbd "i") #'pur-exit)
 (define-key pur-mode-map (kbd "h") #'backward-char)
+(define-key pur-mode-map (kbd "j") #'next-line)
+(define-key pur-mode-map (kbd "k") #'previous-line)
 (define-key pur-mode-map (kbd "l") #'forward-char)
-(define-key pur-mode-map (kbd "j") #'forward-word)
-(define-key pur-mode-map (kbd "k") #'backward-word)
 (define-key pur-mode-map (kbd "n") #'next-line)
 (define-key pur-mode-map (kbd "p") #'previous-line)
 (define-key pur-mode-map (kbd "a") #'move-beginning-of-line)
 (define-key pur-mode-map (kbd "e") #'move-end-of-line)
 (define-key pur-mode-map (kbd "c") #'pur-change)
-(define-key pur-mode-map (kbd "d") #'delete-char)
 (define-key pur-mode-map (kbd "s") #'expreg-expand)
 (define-key pur-mode-map (kbd "v") #'set-mark-command)
 (define-key pur-mode-map (kbd "u") #'undo)
+(define-key pur-mode-map (kbd "o") #'pur-insert-below)
+(define-key pur-mode-map (kbd "O") #'pur-insert-above)
+(define-key pur-mode-map (kbd "f") #'forward-word)
+(define-key pur-mode-map (kbd "b") #'backward-word)
+(define-key pur-mode-map (kbd "g") #'pur-g)
