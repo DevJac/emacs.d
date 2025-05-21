d6dc24456e081eaf893da90e766bf599963ab090
DevJac <dbuttonz@gmail.com>
2025-05-21 12:37:04 -0600

    Move purr-more.el into my.el

--- init.el
+++ init.el
@@ -38,9 +38,8 @@
 ;; (setq use-package-compute-statistics t)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(load "my.el")
 (load "purr.el")
-(load "purr-more.el")
+(load "my.el")
 (global-purr-mode 1)
 
 (use-package consult
--- lisp/my.el
+++ lisp/my.el
@@ -1,8 +1,20 @@
 (defun my/complete-in-minibuffer ()
+  "Override the current `completion-in-region-function' and use `consult-completion-in-region'.
+
+This is helpful if you want to move a completion in the main buffer into the minibuffer."
   (interactive)
   (let ((completion-in-region-function #'consult-completion-in-region))
     (completion-at-point)))
 
 (defun my/enable-babel-evaluate ()
+  "By default babel code blocks will ask for confirmation before evaluating. Disable that."
   (interactive)
   (setq org-confirm-babel-evaluate nil))
+
+(defun my/org-table-change-field ()
+  "Blank the org table field then begin inserting text."
+  (interactive)
+  (org-table-blank-field)
+  (purr-exit))
+
+(define-key purr-mode-map (kbd "SPC o t c") #'purr-org-table-change-field)
