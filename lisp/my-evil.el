f7cb587e926d0235456a1910273a05c174cfa378
DevJac <dbuttonz@gmail.com>
2025-05-22 22:14:59 -0600

    my-evil.el

---
+++ lisp/my-evil.el
@@ -0,0 +1,52 @@
+(use-package goto-chg
+  :ensure t)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-search-module 'evil-search)
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+(use-package evil-anzu
+  :ensure t
+  :config
+  (global-anzu-mode 1))
+(use-package doom-modeline
+  :ensure t
+  :config
+  (doom-modeline-mode 1))
+
+;;; evil keybinds
+
+(defun kill-buffer-and-window-quiet ()
+  "Kill current buffer, and kill window if there is more than one."
+  (interactive)
+  (kill-buffer (current-buffer))
+  (unless (one-window-p)
+    (evil-window-delete)))
+
+(defun save-then-kill-buffer-and-window-quiet ()
+  "Save and kill current buffer, and kill window if there is more than one."
+  (interactive)
+  (if (buffer-file-name)
+      (save-buffer))
+  (kill-buffer-and-window-quiet))
+
+;; Don't allow evil commands to quit Emacs.
+(evil-ex-define-cmd "q[uit]" #'kill-buffer-and-window-quiet)
+(evil-ex-define-cmd "wq" #'save-then-kill-buffer-and-window-quiet)
+(evil-define-key '(motion normal) 'global
+  (kbd "ZQ") #'kill-buffer-and-window-quiet
+  (kbd "ZZ") #'save-and-then-kill-buffer-and-window-quiet)
+
+;; Create a text object between |'s. This is especially useful for org tables.
+(evil-define-text-object evil-org-table-cell-inner
+  (count &optional beg end type)
+  "Select the contents of an org-mode table cell, excluding the | delimiters."
+  (evil-select-paren "\\|" "\\|" beg end type count))
+
+(define-key evil-inner-text-objects-map "|" #'evil-org-table-cell-inner)
