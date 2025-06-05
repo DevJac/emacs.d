0decf9d1d498a880c48ffe43195f48cee18a8060
DevJac <dbuttonz@gmail.com>
2025-06-04 21:26:40 -0600

    evil-keys: SPC p f, SPC p g for finding files and grepping

--- lisp/my-evil.el
+++ lisp/my-evil.el
@@ -71,6 +71,8 @@
   (kbd "<leader> V") #'expreg-contract
   (kbd "<leader> P") #'consult-yank-from-kill-ring
   (kbd "<leader> t t") #'show-trailing-whitespace
+  (kbd "<leader> p f") #'project-find-file
+  (kbd "<leader> p g") #'consult-ripgrep
   (kbd "<leader> o s") #'insert-timestamp
   (kbd "<leader> o h") #'org-insert-heading-promote
   (kbd "<leader> o j") #'org-insert-heading-down
