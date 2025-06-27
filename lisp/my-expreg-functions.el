e2f059dc8fbdc5fde6bf2b16cef71fd5ffe31b49
DevJac <dbuttonz@gmail.com>
2025-06-26 22:30:43 -0600

    Match all org blocks with expreg function

--- lisp/my-expreg-functions.el
+++ lisp/my-expreg-functions.el
@@ -8,16 +8,21 @@
             (end (progn (org-table-end-of-field 0) (point))))
         (list `(org-table-field . ,(cons beg end)))))))
 
-(defun my/expreg--org-src-block ()
+(defun my/expreg--org-block ()
   "Identify src blocks for expreg."
-  (when (and (derived-mode-p 'org-mode)
-             (org-in-src-block-p))
-    (save-mark-and-excursion
-      (org-babel-mark-block)
-      (let ((beg (region-beginning))
-            (end (region-end)))
-        (list `(org-babel-src-block . ,(cons beg end)))))))
+  (save-excursion
+    (let ((case-fold-search t)
+          (orig-point (point))
+          block-beg block-end)
+      (search-backward "#+begin_" nil 'NOERROR)
+      (next-line)
+      (setq block-beg (point))
+      (search-forward "#+end_" nil 'NOERROR)
+      (beginning-of-line)
+      (setq block-end (point))
+      (when (and (<= block-beg orig-point) (<= orig-point block-end))
+        (list `(org-block . ,(cons block-beg block-end)))))))
 
 (add-to-list 'expreg-functions #'my/expreg--org-table-field)
-(add-to-list 'expreg-functions #'my/expreg--org-src-block)
+(add-to-list 'expreg-functions #'my/expreg--org-block)
 (setq-default expreg-functions expreg-functions)
