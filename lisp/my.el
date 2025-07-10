dc6b12c91da2d2a40b1bb7d8e8bc469e929661d1
DevJac <dbuttonz@gmail.com>
2025-07-10 14:51:53 -0600

    SPC p a h and related keybinds use expand-file-name for portability

--- lisp/my.el
+++ lisp/my.el
@@ -17,13 +17,24 @@ This is helpful if you want to move a completion in the main buffer into the min
   (setq show-trailing-whitespace t))
 
 (defun find-file-rec (&rest paths)
-  "Fuzzy find on ALL files in the home directory."
+  "Fuzzy find on ALL files in the given directories."
   (find-file
    (completing-read "Find file: "
                     ;; Using #'process-lines-ignore-status here
                     ;; might help with debugging.
                     (apply #'process-lines "fd" "-u" "." paths))))
 
-(defun find-file-home () (interactive) (find-file-rec "/home/devjac"))
-(defun find-file-code () (interactive) (find-file-rec "/home/devjac/Code"))
-(defun find-file-emacs () (interactive) (find-file-rec "/home/devjac/.emacs.d"))
+(defun find-file-home ()
+  "Fuzzy find on ALL files in the home directory."
+  (interactive)
+  (find-file-rec (expand-file-name "~")))
+
+(defun find-file-code ()
+  "Fuzzy find on ALL files in the Code directory."
+  (interactive)
+  (find-file-rec (expand-file-name "~/Code")))
+
+(defun find-file-emacs ()
+  "Fuzzy find on ALL files in the .emacs.d directory."
+  (interactive)
+  (find-file-rec (expand-file-name "~/.emacs.d")))
