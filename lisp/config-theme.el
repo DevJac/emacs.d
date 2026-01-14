dcb80ca26335dbb140351481099d5987b3e503f4
DevJac <dbuttonz@gmail.com>
2026-01-14 16:48:43 -0700

    Adjust gruvbox-theme highlight color with math

--- lisp/config-theme.el
+++ lisp/config-theme.el
@@ -2,9 +2,16 @@
   :ensure t)
 (use-package gruvbox-theme
   :ensure t)
+(use-package rainbow-mode
+  :ensure t)
+(use-package ct
+  :ensure t)
 
 (load-theme 'gruvbox-dark-hard 'noconfirm)
 
 (when (member 'gruvbox-dark-hard custom-enabled-themes)
   (set-face-attribute 'highlight nil
-                      :background "#403933"))
+                      :background
+                      (color-darken-name
+                       (face-attribute 'highlight :background)
+                       40)))
