9b847c653c3186a7ba4e7e4939c10d2d836d9d61
DevJac <dbuttonz@gmail.com>
2026-03-06 23:34:27 -0700

    Enable evaluation of lua blocks (with org babel)

--- lisp/config-lua.el
+++ lisp/config-lua.el
@@ -1,2 +1,11 @@
 (use-package lua-mode
-  :ensure t)
+  :ensure t
+  :init
+  (setq lua-indent-level 4))
+
+;; Enable org babel evaluation for Lua.
+(require 'ob-lua)
+
+;; Alternatively,
+; (add-to-list 'org-babel-load-languages '(lua . t))
+; (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
